5f6f8ba60debbfacb7edf27c94cdfb56f39ff73b
neohun <neohunn@yandex.ru>
2023-04-26 17:14:01 +0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AbandonedBeach/testmod2
+
+go 1.20
---
+++ testmod.go
@@ -0,0 +1,7 @@
+package testmod2
+
+import "fmt"
+
+func Greeting(name string) string {
+	return fmt.Sprintf("Hi, %v. Welcome!", name)
+}
